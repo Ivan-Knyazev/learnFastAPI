dac62df54238888d574f79e68ba591232418c4db
Ivan Knyazev <Ivan.Knyazev.2005@yandex.ru>
2024-02-02 18:28:46 +0400

    add routes for get info about `products`

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request, Body
+from fastapi import FastAPI, Request, Body, HTTPException
 from fastapi.templating import Jinja2Templates
 
 from app.models.models import *
@@ -63,6 +63,29 @@ async def get_all_feedback():
 @app.post("/create_user", response_model=UserCreate)
 async def create_user(user: UserCreate):
     return user
+
+
+@app.get("/product/{product_id}", response_model=Product)
+async def get_product(product_id: int):
+    for product in sample_products:
+        if product_id == product["product_id"]:
+            return product
+    else:
+        raise HTTPException(status_code=404, detail="Product not found!")
+
+
+def find_product(keyword: str, category: str | None, limit: int):
+    products = []
+    for product in sample_products:
+        if keyword.lower() in product["name"].lower():
+            if category is None or product["category"] == category:
+                products.append(product)
+    return products[:limit]
+
+
+@app.get("/products/search", response_model=list[Product])
+async def get_product(keyword: str, category: str | None = None, limit: int = 10):
+    return find_product(keyword, category, limit)
 
 
 if __name__ == "__main__":
--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+from unicodedata import category
+
 
 class User(BaseModel):
     id: int
@@ -26,3 +28,49 @@ class Feedback(BaseModel):
 
 class FeedbackResponse(BaseModel):
     message: str
+
+
+# Products
+sample_product_1 = {
+    "product_id": 123,
+    "name": "Smartphone",
+    "category": "Electronics",
+    "price": 599.99
+}
+
+sample_product_2 = {
+    "product_id": 456,
+    "name": "Phone Case",
+    "category": "Accessories",
+    "price": 19.99
+}
+
+sample_product_3 = {
+    "product_id": 789,
+    "name": "Iphone",
+    "category": "Electronics",
+    "price": 1299.99
+}
+
+sample_product_4 = {
+    "product_id": 101,
+    "name": "Headphones",
+    "category": "Accessories",
+    "price": 99.99
+}
+
+sample_product_5 = {
+    "product_id": 202,
+    "name": "Smartwatch",
+    "category": "Electronics",
+    "price": 299.99
+}
+
+sample_products = [sample_product_1, sample_product_2, sample_product_3, sample_product_4, sample_product_5]
+
+
+class Product(BaseModel):
+    product_id: int
+    name: str
+    category: str
+    price: float
